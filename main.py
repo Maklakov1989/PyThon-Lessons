93dc37d023479739b70db27c4e2e73728b3f7207
Alexey M <maklakov_a.n@mail.ru>
2022-03-08 14:09:48 +0300

    Задания 1 - 5

---
+++ main.py
@@ -0,0 +1,56 @@
+# print(('*' * 25), 'Задание №1', ('*' * 25))
+#
+# spisok = [2, 'слово', {'словарь'}, ['список']]
+# print(spisok)
+# for element in spisok:
+#     print(element, '--', type(element))
+# print(('*' * 25), 'Задание №2', ('*' * 25))
+#
+# my_list = input('Введите несколько чисел через пробел: ')
+# my_list = my_list.split(' ')
+# my_list = list(my_list)
+# print(type(my_list))
+# print(my_list)
+# for i in range(len(my_list)):
+#      tmp = my_list[i]
+#      my_list[i] = len(my_list[0] - 1)
+
+# print(('*' * 25), 'Задание №3', ('*' * 25))
+# month = (input('Введите число от 1 до 12: '))
+# mm = {'1':'Зима', '2':'Зима','3':'Весна','4':'Весна','5':'Весна','6':'Лето','07':'Лето','8':'Лето','9':'Осень',
+#       '10':'Осень','11':'Осень','12':'Зима'
+# }
+# print('это будет - ', mm[month])
+# print(('*' * 25), 'Задание №4', ('*' * 25))
+# words = input('Введите несколько слов через пробел: ')
+# words_list = words.split(' ')
+# count = 0
+# for i in range(1, len(words_list)+1):
+#     print(i, ')', words_list[i-1])
+# print(('*' * 25), 'Задание №5', ('*' * 25))
+# my_list = [7, 5, 3, 3, 2]
+# print(my_list)
+# y = None
+# x = 1
+# for x in my_list:
+#     x = int(input('Введите число: '))
+#     my_list.append(x)
+#     y = (sorted(my_list))
+#     print(f'Пользователь ввёл число {x}, Результат: {y}')
+
+# print(('*' * 25), 'Задание №6', ('*' * 25))
+# x = input('Введите тип товара: ')
+# y = input('Введите цену товара: ')
+# z = input('Введите количество товара: ')
+# a = None
+# a = list
+# b = a.append(x, y, z)
+# print(b)
+# for i in range(1, len(words_list)+1):
+#      print(i, ')', words_list[i-1])
+
+# [
+# (1, {“Тип товара”: None, “цена”: None, “количество”: None, “eд”: None}),
+# (2, {“название”: “принтер”, “цена”: 6000, “количество”: 2, “eд”: “шт.”}),
+# (3, {“название”: “сканер”, “цена”: 2000, “количество”: 7, “eд”: “шт.”})
+# ]
