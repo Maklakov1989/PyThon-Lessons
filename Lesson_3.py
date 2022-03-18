4934c47cd4862ae958e0a5bef31389d80ab24e94
Alexey <maklakov_a.n@mail.ru>
2022-03-18 14:21:44 +0300

    Небольшие правки по оформлению

--- Lesson_3.py
+++ Lesson_3.py
@@ -1,49 +1,49 @@
-print(('*' * 25), 'Задание №1', ('*' * 25))
-
-def devide(number_1, number_2):
-    try:
-        return number_1 / number_2
-    except: ZeroDivisionError
-number_1 = int(input('Введите 1 число: '))
-number_2 = int(input('Введите 2 число: '))
-print(f'Число 1 делить на число 2 равно - {devide(number_1, number_2)}')
-print(('*' * 25), 'Задание №2', ('*' * 25))
-def person():
-    return print(name,surname, date_of_birth, city, e_mail, tel)
-name = input('Введите имя: ')
-surname = input('Введите фамилию: ')
-date_of_birth = input('Введите дату рождения: ')
-city = input('Введите место рождения: ')
-e_mail = input('Введите e-mail: ')
-tel = input('Введите телефон: ')
-person()
-print(('*' * 25), 'Задание №3', ('*' * 25))
-def max_func():
-    s = max(x)
-    x.remove(s)
-    s2 = max(x)
-    print(s, s2)
-    return s + s2
-a = int(input('Введите занение 1: '))
-b = int(input('Введите занение 2: '))
-c = int(input('Введите занение 3: '))
-x = [a, b, c]
-print(f'Сумма максимальных 2-х значений из введённых - {max_func()}')
-print(('*' * 25), 'Задание №4', ('*' * 25))
-x = int(input('Введите действительное положительное число: '))
-y = int(input('Введите целое отрицательное число: '))
-"""Возведение в степень встроенной командой"""
-def my_func_1 (x, y):
-        return x**y
-print(f'Результат функции 1: {my_func_1(x, y)}')
-"""Возведение в степень без встроенной команды"""
-i = 1
-def my_func_2 (x, y):
-    for i in range(abs(x)):
-        i = y
-        i = 1/(x * x)
-        return i
-print(f'Результат функции 2: {my_func_2(x, y)}')
+# print(('*' * 25), 'Задание №1', ('*' * 25))
+#
+# def devide(number_1, number_2):
+#     try:
+#         return number_1 / number_2
+#     except: ZeroDivisionError
+# number_1 = int(input('Введите 1 число: '))
+# number_2 = int(input('Введите 2 число: '))
+# print(f'Число 1 делить на число 2 равно - {devide(number_1, number_2)}')
+# print(('*' * 25), 'Задание №2', ('*' * 25))
+# def person():
+#     return print(name,surname, date_of_birth, city, e_mail, tel)
+# name = input('Введите имя: ')
+# surname = input('Введите фамилию: ')
+# date_of_birth = input('Введите дату рождения: ')
+# city = input('Введите место рождения: ')
+# e_mail = input('Введите e-mail: ')
+# tel = input('Введите телефон: ')
+# person()
+# print(('*' * 25), 'Задание №3', ('*' * 25))
+# def max_func():
+#     s = max(x)
+#     x.remove(s)
+#     s2 = max(x)
+#     print(s, s2)
+#     return s + s2
+# a = int(input('Введите занение 1: '))
+# b = int(input('Введите занение 2: '))
+# c = int(input('Введите занение 3: '))
+# x = [a, b, c]
+# print(f'Сумма максимальных 2-х значений из введённых - {max_func()}')
+# print(('*' * 25), 'Задание №4', ('*' * 25))
+# x = int(input('Введите действительное положительное число: '))
+# y = int(input('Введите целое отрицательное число: '))
+# """Возведение в степень встроенной командой"""
+# def my_func_1 (x, y):
+#         return x**y
+# print(f'Результат функции 1: {my_func_1(x, y)}')
+# """Возведение в степень без встроенной команды"""
+# i = 1
+# def my_func_2 (x, y):
+#     for i in range(abs(x)):
+#         i = y
+#         i = 1/(x * x)
+#         return i
+# print(f'Результат функции 2: {my_func_2(x, y)}')
 
 print(('*' * 25), 'Задание №5', ('*' * 25))
 numbers = input('Введите числа, разделённые пробелом: ')
@@ -59,20 +59,22 @@ for number in spisok:
     total = total + number
 print(total)
 number_add = 0
-def function_input(total):
-    while (number_add == 'нет') == True:
-        number_add = input('Введите числа, разделённые пробелом: ')
-        number_add = number_add.split(' ')
-        number_add = list(number_add)
-        spisok_2 = []
-        for i in number_add:
-            i = int(i)
-            spisok_2.append(i)
-        print(spisok)
-        total_2 = 0
-        for number in spisok_2:
-            total_2 = total + number
-        print(total_2)
 
-function_input(total)
+
+# def function_input(total):
+#     while (number_add == 'нет') == True:
+#         number_add = input('Можете числа, разделённые пробелом, если напишите слово Нет, то цикл прекратится: ')
+#         number_add = number_add.split(' ')
+#         number_add = list(number_add)
+#         spisok_2 = []
+#         for i in number_add:
+#             i = int(i)
+#             spisok_2.append(i)
+#         print(spisok)
+#         total_2 = 0
+#         for number in spisok_2:
+#             total_2 = total + number
+#         print(total_2)
+#
+# function_input(total)
 
