1fb5e707ac5dbbc180e9d1d4579f4bd50c824dea
Alexey <maklakov_a.n@mail.ru>
2022-03-24 10:18:38 +0300

    Задание 4 выполнено

--- Lesson_4.py
+++ Lesson_4.py
@@ -8,15 +8,15 @@ salary = (int(hours) * int(rate)) + int(bonus)
 print(f'Заработная плата в месяц {salary}')
 print(('*' * 25), 'Задание №2 к лекции № 4 НЕ ГОТОВО!!!!', ('*' * 25))
 from random import randint
-from functools import reduce
 my_list = []
 for i in range(12):
     my_list.append(randint(1, 10))
+my_list = [300, 2, 12, 44, 1, 1, 4, 10, 7, 1, 78, 123, 55]
 print(f'Дан случайный список - {my_list}')
-new_list = [el > prev_el for el in my_list]
-
-
-
+new_list = []
+for i in my_list:
+    if my_list[i] > my_list[i - 1]:
+        new_list.append(i)
 print(f'Новый список с элементами больше предудущего: {new_list}')
 print(('*' * 25), 'Задание №3 к лекции № 4', ('*' * 25))
 my_list = range(20,240)
@@ -31,7 +31,11 @@ for i in range(12):
 print(f'Дан случайный список - {my_list}')
 new_list = []
 for el in my_list:
-    if el not in new_list:
+    b = 0
+    for i in my_list:
+        if el == i:
+            b += 1
+    if b == 1:
         new_list.append(el)
 print('Элементы не имеющие повторений - ', new_list)
 print(('*' * 25), 'Задание №5 к лекции № 4', ('*' * 25))
@@ -49,6 +53,4 @@ print(('*' * 25), 'Задание №6 к лекции № 4 НЕ ГОТОВО!!
 from sys import argv
 skript_name, start_number, random_number = argv
 print(('*' * 25), 'Задание №7 к лекции № 4 НЕ ГОТОВО!!!!', ('*' * 25))
-
-
 
