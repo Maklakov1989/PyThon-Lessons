37045646c9c57a6025161e9f5333c730702d6955
Alexey <maklakov_a.n@mail.ru>
2022-03-26 20:04:00 +0300

    Задание 7 выполнено

--- Lesson_4.py
+++ Lesson_4.py
@@ -6,21 +6,13 @@
 # print(f'Премия - {bonus}')
 # salary = (int(hours) * int(rate)) + int(bonus)
 # print(f'Заработная плата в месяц {salary}')
-print(('*' * 25), 'Задание №2 к лекции № 4 НЕ ГОТОВО!!!!', ('*' * 25))
+# print(('*' * 25), 'Задание №2 к лекции № 4 НЕ ГОТОВО!!!!', ('*' * 25))
 # from random import randint
-# my_list = []
-# for i in range(12):
-#     my_list.append(randint(1, 10))
+# my_list = [randint(1, 10) for i in range(12)]
 # my_list = [300, 2, 12, 44, 1, 1, 4, 10, 7, 1, 78, 123, 55]
 # print(f'Дан случайный список - {my_list}')
-# i = 0
-# new_list = []
-# for el in my_list:
-#     if my_list[i] > my_list[i - 1]:
-#         el = i
-#         new_list.append(el)
-
-# new_list = [el for el in my_list if my_list[i] > my_list[i - 1]]
+#
+# new_list = [i if my_list[i] > my_list[i - 1] else 'min' for i in my_list ]
 #
 # print(f'Новый список с элементами больше предыдущего: {new_list}')
 # print(('*' * 25), 'Задание №3 к лекции № 4', ('*' * 25))
@@ -28,21 +20,21 @@ print(('*' * 25), 'Задание №2 к лекции № 4 НЕ ГОТОВО!!
 # new_list = [el for el in my_list if el % 21 == 0 or el % 20 == 0]
 # print(f"Исходный список: {my_list}")
 # print(f"Новый список: {new_list}")
-print(('*' * 25), 'Задание №4 к лекции № 4', ('*' * 25))
-from random import randint
-my_list = []
-for i in range(12):
-    my_list.append(randint(1, 10))
-print(f'Дан случайный список - {my_list}')
-new_list = []
-for el in my_list:
-    b = 0
-    for i in my_list:
-        if el == i:
-            b += 1
-    if b == 1:
-        new_list.append(el)
-print('Элементы не имеющие повторений - ', new_list)
+# print(('*' * 25), 'Задание №4 к лекции № 4', ('*' * 25))
+# from random import randint
+# my_list_4 = [randint(1, 10) for i in range(5)]
+# print(f'Дан случайный список - {my_list_4}')
+#
+# new_list = [elem for elem in my_list_4 if my_list_4[i] != my_list_4[i + 1]]
+# new_list = []
+# for el in my_list_4:
+#     b = 0
+#     for i in my_list_4:
+#         if el == i:
+#             b += 1
+#     if b == 1:
+#         new_list.append(el)
+# print('Элементы не имеющие повторений - ', new_list)
 # print(('*' * 25), 'Задание №5 к лекции № 4', ('*' * 25))
 # from functools import reduce
 # from random import randint
@@ -54,18 +46,31 @@ print('Элементы не имеющие повторений - ', new_list)
 #     """prev_el - предыдущий элемент, el - текущий элемент"""
 #     return prev_el + el
 # print(f'Произведение всех элементов списка - ', reduce(my_func, my_list))
-# print(('*' * 25), 'Задание №6 к лекции № 4 НЕ ГОТОВО!!!!', ('*' * 25))
+print(('*' * 25), 'Задание №6 к лекции № 4 НЕ ГОТОВО!!!!', ('*' * 25))
 # from sys import argv
 # skript_name, start_number, random_number = argv
 # start_number = int(start_number)
 # random_number = int(random_number)
-# from random import randint
 # for number in range(start_number, random_number):
 #     print(number)
+# from sys import argv
+# skript_name, start_number, random_number = argv
+# from random import randint
+# my_list = [randint(1, 10) for i in range(12)]
+# from itertools import count
+# for el in count(7):
+# if el > 15:
+#
 
-
-
-
-
-# print(('*' * 25), 'Задание №7 к лекции № 4 НЕ ГОТОВО!!!!', ('*' * 25))
-
+print(('*' * 25), 'Задание №7 к лекции № 4', ('*' * 25))
+n = int(input('Введите число факториала: '))
+result = 1
+def generator(n):
+    global result
+    for el in range(1, n + 1):
+        result = result * el
+        yield result
+g = generator(n)
+print(g)
+for x in g:
+    print(x)
