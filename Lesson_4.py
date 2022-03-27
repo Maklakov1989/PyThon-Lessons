2b14cab23eec196367f7c7c08b2053ff6855ea8a
Alexey <maklakov_a.n@mail.ru>
2022-03-27 17:15:03 +0300

    Задание 6 выполнено

--- Lesson_4.py
+++ Lesson_4.py
@@ -9,7 +9,7 @@ print(f'Заработная плата в месяц {salary}')
 print(('*' * 25), 'Задание №2 к лекции № 4', ('*' * 25))
 from random import randint
 my_list = [randint(1, 10) for i in range(12)]
-my_list = [300, 2, 12, 44, 1, 1, 4, 10, 7, 1, 78, 123, 55]
+
 print(f'Дан случайный список - {my_list}')
 new_list = [el for el in my_list if el > my_list[my_list.index(el) - 1]]
 print(f'Новый список с элементами больше предыдущего: {new_list}')
@@ -27,29 +27,26 @@ print('Элементы не имеющие повторений - ', new_list)
 print(('*' * 25), 'Задание №5 к лекции № 4', ('*' * 25))
 from functools import reduce
 from random import randint
-my_list = []
-for i in range(12):
-    my_list.append(randint(100, 1000))
+my_list = [randint(1, 10) for i in range(12)]
 print(f'Дан случайный список - {my_list}')
 def my_func(prev_el, el):
     """prev_el - предыдущий элемент, el - текущий элемент"""
     return prev_el + el
 print(f'Произведение всех элементов списка - ', reduce(my_func, my_list))
-print(('*' * 25), 'Задание №6 к лекции № 4 НЕ ГОТОВО!!!!', ('*' * 25))
+print(('*' * 25), 'Задание №6 к лекции № 4', ('*' * 25))
 from sys import argv
 skript_name, start_number, random_number = argv
 start_number = int(start_number)
 random_number = int(random_number)
 for number in range(start_number, random_number):
     print(number)
-from sys import argv
-skript_name, start_number, random_number = argv
-from random import randint
-my_list = [randint(1, 10) for i in range(12)]
-from itertools import count
-for el in count(7):
-if el > 15:
-
+from itertools import cycle
+my_list_6 = [2, 12, 44, 1, 1, 4, 10, 7, 1, 300, 123, 55]
+print(f'Дан список - {my_list_6}')
+for el in cycle(my_list_6):
+    print(el)
+    if el == my_list_6[my_list_6.index(el) + 1]:
+        break
 print(('*' * 25), 'Задание №7 к лекции № 4', ('*' * 25))
 n = int(input('Введите число факториала: '))
 result = 1
