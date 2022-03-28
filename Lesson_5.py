c0d86d3d48f117ebdaa11900f76e8ed0ebb39397
Alexey <maklakov_a.n@mail.ru>
2022-03-28 15:25:21 +0300

    Задание 2 выполнено полностью

--- Lesson_5.py
+++ Lesson_5.py
@@ -10,7 +10,7 @@ f_obj = open("HomeWork.txt", 'a')
 for line in text:
     f_obj.write(line + '\n')
 f_obj.close()
-print(('*' * 25), 'Задание №2 к лекции № 5(не доделал)', ('*' * 25))
+print(('*' * 25), 'Задание №2 к лекции № 5', ('*' * 25))
 my_f = open('HW_2_2.txt', 'r', encoding='utf-8')
 content = my_f.readlines()
 print(content)
