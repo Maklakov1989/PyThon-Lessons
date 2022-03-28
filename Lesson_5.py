8ee66731ecfcc36f5bd30bfb85b7f84e869a97a5
Alexey <maklakov_a.n@mail.ru>
2022-03-28 18:32:21 +0300

    Задание 5 выполнено полностью

--- Lesson_5.py
+++ Lesson_5.py
@@ -18,16 +18,32 @@ lines = (len(content))
 print('Строк в файле - ', lines)
 a = str(content)
 print('Количество слов в тексте - ', len(set(a.split())))
-
+f_obj.close()
 print(('*' * 25), 'Задание №3 к лекции № 5(не доделал)', ('*' * 25))
-
 f_salary = open('HW_2_3.txt', 'r', encoding='utf-8')
-content = f_salary.read()
-print(content)
-new_list = []
-for line in content:
-    new_list.append(line)
-print(new_list)
+list = []
+data = []
+for el_1 in f_salary.readlines():
+    el_2 = el_1.split(' ')
+    list.append(el_2)
+print(('*' * 25), 'Задание №4 к лекции № 5(не доделал)', ('*' * 25))
+print(('*' * 25), 'Задание №5 к лекции № 5', ('*' * 25))
+numbers = input('Введите числа через пробел: ')
+f_obj = open("HW_2_5.txt", 'a')
+f_obj.write('Пользователь ввёл чиcла - ' + numbers + '\n')
+numbers = list(numbers.split(' '))
+print('Вы ввели: ', numbers)
+sum_list = []
+for i in numbers:
+    i = int(i)
+    sum_list.append(i)
+from functools import reduce
+def my_func(prev_el, el):
+    return prev_el + el
+x = reduce(my_func, sum_list)
+print('Сумма всех элементов', x)
+f_obj.write(f'Сумма всех элементов {x}')
+f_obj.close()
 
 
 
