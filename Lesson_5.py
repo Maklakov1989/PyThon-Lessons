4ceb2efc4c7196bc26aa37fb49fa44e75885b4e6
Alexey <maklakov_a.n@mail.ru>
2022-03-28 13:43:45 +0300

    Задание 2 выполнено наплоловину

--- Lesson_5.py
+++ Lesson_5.py
@@ -10,4 +10,13 @@ f_obj = open("HomeWork.txt", 'a')
 for line in text:
     f_obj.write(line + '\n')
 f_obj.close()
+print(('*' * 25), 'Задание №2 к лекции № 5(не доделал)', ('*' * 25))
+my_f = open('HW_2.txt', 'r', encoding='utf-8')
+content = my_f.readlines()
+print(content)
+lines = (len(content))
+print('Строк в файле - ', lines)
+
+
+
 
