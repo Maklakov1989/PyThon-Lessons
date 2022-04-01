8be712b5b3343ec25cb8daf2e151f11dc451c82e
Alexey <maklakov_a.n@mail.ru>
2022-04-01 19:01:58 +0300

    Начато задание 1

--- Lesson_7.py
+++ Lesson_7.py
@@ -1,1 +1,12 @@
+print(('*' * 25), 'Задание №1 к лекции № 7', ('*' * 25))
+class Matrix:
+    def __init__(self, param_1, param_2):
+        self.param_1 = param_1
+        self.param_2 = param_2
+    def spisok:
+        self.param_1 = list_1
+        el = None
+        el for el in list_1
+
+
 
