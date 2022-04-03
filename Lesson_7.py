ffc7a9f11bf10a1bcf085315723472406649938a
Alexey <maklakov_a.n@mail.ru>
2022-04-03 16:45:29 +0300

    Задание 2 начато

--- Lesson_7.py
+++ Lesson_7.py
@@ -6,11 +6,21 @@ class Matrix:
         self.param_3 = param_3
     def spisok(self):
         self.my_list = [self.param_1, self.param_2, self.param_3]
-        for el in self.my_list:
-            print(el)
+        print(self.my_list)
     def __str__(self):
-        return f'Выми были введены 2 числа {self.param_1}, {self.param_2} и {self.param_3} из которых в случайном порядке ' \
-               f'была сделана матрица 2х4'
-m = Matrix([5, 8], [6, 9], [4, 1])
-print(m.spisok())
-print(m.__str__())
+        return f'\n{self.param_1}, {self.param_2}, {self.param_3}'
+    def __add__(self, other):
+        return Matrix(self.param_1 + other.param_1,
+                      self.param_2 + other.param_2,
+                      self.param_3 + other.param_3)
+m = Matrix(5, 8, 9)
+m2 = Matrix(6, 9, 2)
+m3 = Matrix(2, 4, 1)
+print(m.spisok(), m2.spisok(), m3.spisok())
+print(m.__str__(), m2.__str__(), m3.__str__())
+print(m + m2 + m3)
+print(('*' * 25), 'Задание №2 к лекции № 7', ('*' * 25))
+class Clothes:
+    def __init__(self, param_1, param_2):
+        self.param_1 = param_1
+        self.param_2 = param_2
