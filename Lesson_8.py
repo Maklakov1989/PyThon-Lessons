67de2c87f585b02b69706e51737d836126637a90
Alexey <maklakov_a.n@mail.ru>
2022-04-07 16:32:36 +0300

    Задание 1 исправлено

--- Lesson_8.py
+++ Lesson_8.py
@@ -15,17 +15,63 @@ class Date:
                    'ноября': "11", 'декабря': "12"}
         month = my_dict_2[x[1]]
         print(f'Введённый пользователем месяц - {month}')
-Date.get_day(("десятое-декабря-2010"))
-print(Date.get_month("десятое-декабря-2010"))
-print(('*' * 25), 'Задание №2 к лекции № 8', ('*' * 25))
-class my_exeption(ZeroDivisionError):
-    def __init__(self, param):
-        self.param = param
-    try:
-        param = input('Введите число, которое мы будем делить на 0: ')
-        y = param / 0
-    except:
-        print("А делить на ноль нельзя!!")
-print(('*' * 25), 'Задание №3 к лекции № 8', ('*' * 25))
-class my_exeption(ZeroDivisionError):
-    try:
+Date.get_day("десятое-декабря-2010")
+Date.get_month("десятое-декабря-2010")
+# print(('*' * 25), 'Задание №2 к лекции № 8', ('*' * 25))
+# class my_exeption(ZeroDivisionError):
+#     def __init__(self, param):
+#         self.param = param
+#     try:
+#         param = input('Введите число, которое мы будем делить на 0: ')
+#         y = param / 0
+#     except:
+#         print("А делить на ноль нельзя!!")
+# print(('*' * 25), 'Задание №3 к лекции № 8', ('*' * 25))
+# class Str_exeption(Exception):
+#     def __init__(self, list):
+#         self.list = list
+# inp_data = input("Введите любые значения (цифры, слова, буквы): ")
+# inp_data = inp_data.split(' ')
+# inp_data = list(inp_data)
+# data = []
+# try:
+#     for el in inp_data:
+#         if type(el) == int:
+#             data.append(el)
+#             raise Str_exeption("В списке есть слова")
+# except
+#
+#
+# print('Хотите продолжить? y/n')
+#     a = input()
+#     f = []
+#     while a == 'y':
+#         print('Введите числа через пробел: ')
+#         d =(input())
+#         d = d.split(' ')
+#         d = list(d)
+#         for i in d:
+#             i = int(i)
+#             f.append(i)
+#         print('Список новых чисел - ', f)
+#         for number in f:
+#             total = total + number
+#         print(f'Cумма всех введённых чисел - {total}')
+#         print('Хотите продолжить y/n')
+#         a = input()
+#     if a == 'n':
+#         print('Задание завершено! Спасибо!!')
+print(('*' * 25), 'Задание №4 к лекции № 8', ('*' * 25))
+class Warehouse:
+    def __init__(self, units):
+         self.units = units
+class Equipment:
+    def __init__(self, type):
+         self.type = type
+class printer(Equipment):
+    def __init__(self, type):
+        self.type = type
+class scaner(Equipment):
+    pass
+class xerox(Equipment):
+    pass
