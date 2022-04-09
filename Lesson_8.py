4784e54be27d6e7c8d4321eadad3b564fa089c4c
Alexey <maklakov_a.n@mail.ru>
2022-04-09 23:08:07 +0300

    Задание 4,5 выполнено

--- Lesson_8.py
+++ Lesson_8.py
@@ -55,24 +55,52 @@ while a == 'y':
     a = input()
     if a == 'n':
         print('Задание завершено! Спасибо!!')
-#
-# print(('*' * 25), 'Задание №4 к лекции № 8', ('*' * 25))
-# class Warehouse:
-#     def __init__(self, quonts):
-#          self.quonts = quonts
-# class Equipment:
-#     def __init__(self, units, type):
-#         self.units = units
-#         self.type = type
-#         to_wh = {}
-# class printer(Equipment):
-#     def __init__(self, type, quont):
-#         self.type = type
-#         self.quont = quont
-#         to_wh[self.type] = self.quont
-# class scaner(Equipment):
-#     def __init__(self, type):
-#         self.type = type
-# class xerox(Equipment):
-#     def __init__(self, type):
-#         self.type = type
+print(('*' * 25), 'Задание №4 к лекции № 8', ('*' * 25))
+class Sklad:
+    def __init__(self):
+        self._dict = {}
+    def add_to(self, equipment):
+        ''' добавляем в словарь обьект по его названию, в значении
+        будет список экземпляров этого оборудования'''
+        self._dict.setdefault(equipment.name, []).append(equipment)
+    def extract(self, name):
+        ''' извлекаем из значения обьект по названию группы.
+        дальше можно расширить поиск по серии, марки или еще чему либо'''
+        if self._dict[name]:
+            self._dict.setdefault(name).pop(0)
+class Equipment:
+    def __init__(self, name, make, year):
+        self.name = name
+        self.make = make
+        self.year = year
+    def __repr__(self):
+        return f'{self.name}-{self.make}-{self.year}'
+class Printer(Equipment):
+    def __init__(self, series, name, make, year):
+        super().__init__(name, make, year)
+        self.series = series
+    def action(self):
+        return 'Печатает'
+class Scaner(Equipment):
+    def __init__(self, name, make, year):
+        super().__init__(name, make, year)
+    def action(self):
+        return 'Сканирует'
+class Xerox(Equipment):
+    def __init__(self, name, make, year):
+        super().__init__(name, make, year)
+    def action(self):
+        return 'Копирует'
+sklad = Sklad()
+# создаем объект и добавляем
+scaner = Scaner('hp', '321', 90)
+sklad.add_to(scaner)
+scaner = Scaner('hp', '311', 97)
+sklad.add_to(scaner)
+scaner = Scaner('hp', '331', 90)
+sklad.add_to(scaner)
+# выводим склад
+print(sklad._dict)
+# забираем с склада и выводим склад
+sklad.extract('hp')
+print(sklad._dict)
